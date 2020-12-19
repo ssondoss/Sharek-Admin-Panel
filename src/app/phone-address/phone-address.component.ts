2f00081ab02de77d5502c8d87c46abd269a25c83
Yousef J. Alnajjar <1997yousef@gmail.com>
2020-12-19 11:20:47 +0200

    resume

--- src/app/phone-address/phone-address.component.ts
+++ src/app/phone-address/phone-address.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import {
+  AngularFirestore,
+  AngularFirestoreDocument,
+} from '@angular/fire/firestore';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-phone-address',
@@ -8,9 +14,23 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class PhoneAddressComponent implements OnInit {
   phoneAddressForm: FormGroup;
-  constructor(public formBuilder: FormBuilder) {}
+  phones: Observable<any[]>;
+  private itemDoc: AngularFirestoreDocument<any>;
+
+  constructor(
+    public firestore: AngularFirestore,
+    public formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    this.phones = firestore.collection('phone-book').valueChanges();
+  }
 
   ngOnInit(): void {
+    let user = JSON.parse(localStorage.getItem('currentSharekAdmin'));
+    console.log(user);
+    if (user == null || user == undefined || user == {}) {
+      this.router.navigate(['/login']);
+    }
     this.phoneAddressForm = this.formBuilder.group({
       contactName: [
         '',
@@ -25,5 +45,30 @@ export class PhoneAddressComponent implements OnInit {
         ]),
       ],
     });
+  }
+  delete(id: string): void {
+    this.itemDoc = this.firestore.doc<any>('phone-book/' + id.trim());
+    this.itemDoc.delete();
+  }
+  add(): void {
+    console.log('add');
+    let id = this.makeId(20);
+    this.itemDoc = this.firestore.doc<any>('phone-book/' + id);
+    this.itemDoc.set({
+      id: id,
+      name: this.phoneAddressForm.controls['contactName'].value,
+      phone: this.phoneAddressForm.controls['phoneNumber'].value,
+    });
+  }
+
+  makeId(length): string {
+    let result = '';
+    const characters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCPNpUd4dEFCdZxLVsslYlSDYZbm-a3pmw',
+    authDomain: 'sharek-80e70.firebaseapp.com',
+    databaseURL: 'https://sharek-80e70.firebaseio.com',
+    projectId: 'sharek-80e70',
+    storageBucket: 'sharek-80e70.appspot.com',
+    messagingSenderId: '934849859475',
+    appId: '1:934849859475:web:7af1cd6285f5dc18822266',
+    measurementId: 'G-H5N2R4S0CL',
+  },
 };
 
 /*
